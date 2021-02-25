671ccb565aa4c25dbd814cd28b2d68a3c0140923
Tymur Foshch <antariuz@gmail.com>
2021-02-25 08:17:46 +0300

    Working on...

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,8 +1,19 @@
 package app;
 
+import bot.Bot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 public class App {
 
     public static void main(String[] args) {
+
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
 
     }
 
--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -1,5 +1,35 @@
 package bot;
 
-public class Bot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onRegister() {
+
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+
+    }
 
 }
